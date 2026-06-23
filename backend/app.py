3c99d1897300a4ad2c693a4178fd4888d8344bdf
user <Kodra.Abela-25@stud.dhbw-ravensburg.de>
2026-06-23 15:00:53 +0200

    Echo API

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,32 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from models.predict import predict_digit
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "PixelWise Backend laeuft!"
+	return jsonify({
+		"service": "PixelWise API",
+		"status": "running"
+	})
 
-@app.route("/predict")
+
+@app.route("/predict", methods=["GET"])
 def predict():
 	return jsonify(predict_digit())
+
+@app.route("/health", methods=["GET"])
+def health():
+	return jsonify({
+		"status": "healthy"
+	})
+
+@app.route("/echo", methods=["POST"])
+def echo():
+	data = request.json
+	return jsonify({
+		"received": data
+	})
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000)
